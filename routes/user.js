5c986574c02a95eddb1e5594c8072d815ffe450a
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-04-12 16:18:42 +0100

    Clase nº53: Middleware de autenticación

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,9 @@ var express = require('express');
 var userController = require('../controllers/user');
 
 var api = express.Router();
+var mdwAuth = require('../middlewares/authenticated');
 
-api.get('/pruebas-del-controlador', userController.pruebas);
+api.get('/pruebas-del-controlador', mdwAuth.ensureAuth, userController.pruebas);
 api.post('/register', userController.saveUser);
 api.post('/login', userController.login);
 
