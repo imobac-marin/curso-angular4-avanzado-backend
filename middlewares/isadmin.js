5866dd76739b27f9ea4862e9c215c238f4f9615a
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-04-16 09:05:24 +0100

    Clase nº65: Middleware para el rol de administrador

---
+++ middlewares/isadmin.js
@@ -0,0 +1,11 @@
+'use strict'
+
+exports.isAdmin = function (req, res, next) {
+    if (req.user.role !== 'ROLE_ADMIN') {
+        return res.status(200).send({
+            message: 'No tienes privilegios para entrar a esta zona'
+        });
+    }
+
+    next();
+}
--- routes/animal.js
+++ routes/animal.js
@@ -5,6 +5,7 @@ var animalController = require('../controllers/animal');
 
 var api = express.Router();
 var mdwAuth = require('../middlewares/authenticated');
+var mdwAdmin = require('../middlewares/isadmin');
 
 var multipart = require('connect-multiparty');
 var mdwUpload = multipart({
@@ -12,12 +13,12 @@ var mdwUpload = multipart({
 });
 
 api.get('/pruebas-animales', mdwAuth.ensureAuth, animalController.pruebas);
-api.post('/animal', mdwAuth.ensureAuth, animalController.saveAnimal);
+api.post('/animal', [mdwAuth.ensureAuth, mdwAdmin.isAdmin], animalController.saveAnimal);
 api.get('/get-animals', animalController.getAnimals);
 api.get('/get-animal/:id', animalController.getAnimal);
-api.put('/update-animal/:id', mdwAuth.ensureAuth, animalController.updateAnimal);
-api.post('/upload-image-animal/:id', [mdwAuth.ensureAuth, mdwUpload], animalController.uploadImage);
+api.put('/update-animal/:id', [mdwAuth.ensureAuth, mdwAdmin.isAdmin], animalController.updateAnimal);
+api.post('/upload-image-animal/:id', [mdwAuth.ensureAuth, mdwUpload, mdwAdmin.isAdmin], animalController.uploadImage);
 api.get('/get-image-animal-file/:imageFile', animalController.getImageFile);
-api.delete('/get-animal/:id', mdwAuth.ensureAuth, animalController.deleteAnimal);
+api.delete('/get-animal/:id', [mdwAuth.ensureAuth, mdwAdmin.isAdmin], animalController.deleteAnimal);
 
 module.exports = api;
